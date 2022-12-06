f2f4a4ac309864bee4e82f9dfbfb1a2b38357e73
SergeyPodgorny <serkonpod@gmail.com>
2022-12-06 18:16:07 +0300

    unreadable code removed

--- GT_MHR_cosine_volume_heat_generation_UDF_with_other_boundary_conditions.cpp
+++ GT_MHR_cosine_volume_heat_generation_UDF_with_other_boundary_conditions.cpp
@@ -1,9 +1,9 @@
-#include "udf.h" // включение спец. библиотеки пользовательских функций 
-#define PI 3.14 // объявление числа ПИ
-#define L 7.93 // высоты активной зоны
-#define q 9906600 // плотность теплового потока в центре активной зоны
-#define D 1.0894448 // толщина активной зоны
-#define d 0.6 // эффективная добавка
+#include "udf.h"
+#define PI 3.14
+#define L 7.93
+#define q 9906600 
+#define D 1.0894448 
+#define d 0.6
 #define DD 150
 #define Tc 764.15
 #define G1 0.0307632 
@@ -18,112 +18,112 @@
 #define Kg4 1
 #define e 1.2026458
 
-DEFINE_PROFILE(heat_flux_2D, thread, index) // объявление имени используемоего макроса и функции
+DEFINE_PROFILE(heat_flux_2D, thread, index)
 {
-	real z[ND_ND]; // массив, содержащий координаты рассчетного пространства
-	real y; // объявление переменной y для координаты y
-	real x; // объявление переменной x для координаты x
+	real z[ND_ND];
+	real y;
+	real x; 
 	real Z;
 	face_t f;
 
-	begin_f_loop(f,thread) // специальный цикл
+	begin_f_loop(f,thread) 
 	{
 		F_CENTROID(z,f,thread);
-		x = z[0]; // присвоение векторов координат переменным
-		y = z[1]; // 0 - координата x, 1 - координата у, 2 - координата z
+		x = z[0]; 
+		y = z[1];
 		Z = z[2];
-		F_PROFILE(f,thread,index) = (Kq*q*cos((PI*Z)/(D+(2*DD)))*cos((PI*y)/(L+(2*d)))*cos((PI*x)/(D+(2*d))))*e; // расчетная формула
+		F_PROFILE(f,thread,index) = (Kq*q*cos((PI*Z)/(D+(2*DD)))*cos((PI*y)/(L+(2*d)))*cos((PI*x)/(D+(2*d))))*e;
 	}
-	end_f_loop(f,thread) // выход из специального цикла
+	end_f_loop(f,thread)
 }
 
 
-DEFINE_PROFILE(Coolant_Temp, thread, index) // объявление имени используемоего макроса и функции
+DEFINE_PROFILE(Coolant_Temp, thread, index) 
 {
-	real z[ND_ND]; // массив, содержащий координаты рассчетного пространства
+	real z[ND_ND]; 
 	
 	face_t f;
 
-	begin_f_loop(f,thread) // специальный цикл
+	begin_f_loop(f,thread) 
 	{
 		F_CENTROID(z,f,thread);
-		F_PROFILE(f,thread,index) = Kt*Tc; // расчетная формула
+		F_PROFILE(f,thread,index) = Kt*Tc; 
 	}
-	end_f_loop(f,thread) // выход из специального цикла
+	end_f_loop(f,thread) 
 }
 
 
-DEFINE_PROFILE(Mass_Flow_distr_11, thread, index) // объявление имени используемоего макроса и функции
+DEFINE_PROFILE(Mass_Flow_distr_11, thread, index)
 {
-	real z[ND_ND]; // массив, содержащий координаты рассчетного пространства
-	real y; // объявление переменной y для координаты y
-	real x; // объявление переменной x для координаты x
+	real z[ND_ND]; 
+	real y; 
+	real x; 
 	face_t f;
 
-	begin_f_loop(f,thread) // специальный цикл
+	begin_f_loop(f,thread) 
 	{
 		F_CENTROID(z,f,thread);
 	
-		F_PROFILE(f,thread,index) = Kg1*G1; // расчетная формула
+		F_PROFILE(f,thread,index) = Kg1*G1;
 	}
-	end_f_loop(f,thread) // выход из специального цикла
-}
-
-
-
-
-
-
-DEFINE_PROFILE(Mass_Flow_distr_12, thread, index) // объявление имени используемоего макроса и функции
-{
-	real z[ND_ND]; // массив, содержащий координаты рассчетного пространства
-	real y; // объявление переменной y для координаты y
-	real x; // объявление переменной x для координаты x
-	face_t f;
-
-	begin_f_loop(f,thread) // специальный цикл
-	{
-		F_CENTROID(z,f,thread);
-	
-		F_PROFILE(f,thread,index) = Kg2*G2; // расчетная формула
-	}
-
-	end_f_loop(f,thread) // выход из специального цикла
+	end_f_loop(f,thread)
 }
 
 
 
-DEFINE_PROFILE(Mass_Flow_distr_m1, thread, index) // объявление имени используемоего макроса и функции
+
+
+
+DEFINE_PROFILE(Mass_Flow_distr_12, thread, index)
 {
-	real z[ND_ND]; // массив, содержащий координаты рассчетного пространства
-	real y; // объявление переменной y для координаты y
-	real x; // объявление переменной x для координаты x
+	real z[ND_ND]; 
+	real y; 
+	real x; 
 	face_t f;
 
-	begin_f_loop(f,thread) // специальный цикл
+	begin_f_loop(f,thread) 
 	{
 		F_CENTROID(z,f,thread);
 	
-		F_PROFILE(f,thread,index) = Kg3*G3; // расчетная формула
+		F_PROFILE(f,thread,index) = Kg2*G2; 
 	}
-	end_f_loop(f,thread) // выход из специального цикла
+
+	end_f_loop(f,thread)
 }
 
-DEFINE_PROFILE(Mass_Flow_distr_m2, thread, index) // объявление имени используемоего макроса и функции
+
+
+DEFINE_PROFILE(Mass_Flow_distr_m1, thread, index)
 {
-	real z[ND_ND]; // массив, содержащий координаты рассчетного пространства
-	real y; // объявление переменной y для координаты y
-	real x; // объявление переменной x для координаты x
+	real z[ND_ND];
+	real y; 
+	real x; 
 	face_t f;
 
-	begin_f_loop(f,thread) // специальный цикл
+	begin_f_loop(f,thread) 
 	{
 		F_CENTROID(z,f,thread);
 	
-		F_PROFILE(f,thread,index) = Kg4*G4; // расчетная формула
+		F_PROFILE(f,thread,index) = Kg3*G3; 
+	}
+	end_f_loop(f,thread) 
+}
+
+DEFINE_PROFILE(Mass_Flow_distr_m2, thread, index) 
+{
+	real z[ND_ND]; 
+	real y; 
+	real x; 
+	face_t f;
+
+	begin_f_loop(f,thread) 
+	{
+		F_CENTROID(z,f,thread);
+	
+		F_PROFILE(f,thread,index) = Kg4*G4; 
 	}
 	
-	end_f_loop(f,thread) // выход из специального цикла
+	end_f_loop(f,thread)
 }
 
 
--- PWR_cosine_volume_heat_generation_UDF_with_other_boundary_conditions.cpp
+++ PWR_cosine_volume_heat_generation_UDF_with_other_boundary_conditions.cpp
@@ -1,49 +1,25 @@
-#include "udf.h" // ��������� ����. ���������� ���������������� ������� 
-#define PI 3.14 // ���������� ����� ��
-#define L 3.658 // ������ �������� ����
-#define DD 1000 // ������� �������� ����
-#define d 0.06 // ����������� �������
-#define D 0.125 // ������� ��
-#define Tc 565.05 // 623.43 565.05
-#define G 5.1336552 // ����� �������� ������ 
-#define Kq 1.05 // 1.5
-#define Kt 1 // 1.2
-#define Kg 1 // 1.2
-#define q1 270360000 // �������� �������� ������������� �����
-#define q2 391170000 // -|-
-#define q3 500460000 // -|-
-#define q4 597100000 // -|-
-#define q5 673040000 // -|-
-#define q6 729980000 // -|-
-#define q7 764500000 // -|-
-#define q8 775430000 // ������������ �����
+#include "udf.h" 
+#define PI 3.14 
+#define L 3.658 
+#define DD 1000 
+#define d 0.06 
+#define D 0.125 
+#define Tc 565.05 
+#define G 5.1336552 
+#define Kq 1.05 
+#define Kt 1 
+#define Kg 1 
+#define q1 270360000 
+#define q2 391170000 
+#define q3 500460000 
+#define q4 597100000 
+#define q5 673040000 
+#define q6 729980000 
+#define q7 764500000 
+#define q8 775430000 
 
 
-DEFINE_PROFILE(heat_flux_1, thread, index) // ���������� ����� �������������� ������� � �������
-{
-	real z[ND_ND]; // ������, ���������� ���������� ����������� ������������
-	real y; // ���������� ���������� y ��� ���������� y
-	real x; // ���������� ���������� x ��� ���������� x
-	real Z;
-	face_t f;
-
-	begin_f_loop(f,thread) // ����������� ����
-	{
-		F_CENTROID(z,f,thread);
-		x = z[0]; // � - ���������� ���������� (�� ������������, ������ �� ��� ������� ����� ����� �������� q1, q2 � �.�)
-		y = z[1]; // � - ���������� ������;
-		Z = z[2]; // Z - ���������� �������;
-		
-		
-		F_PROFILE(f,thread,index) = Kq*q1*cos((PI*y)/(L+(2*d)));
-			 
-			}
-	end_f_loop(f,thread) // ����� �� ������������ �����
-}
-
-
-
-DEFINE_PROFILE(heat_flux_2, thread, index) // ���������� ����� �������������� ������� � �������
+DEFINE_PROFILE(heat_flux_1, thread, index) 
 {
 	real z[ND_ND]; 
 	real y; 
@@ -51,188 +27,212 @@ DEFINE_PROFILE(heat_flux_2, thread, index) // ���������� �
 	real Z;
 	face_t f;
 
-	begin_f_loop(f,thread) // ����������� ����
+	begin_f_loop(f,thread)
 	{
 		F_CENTROID(z,f,thread);
-		x = z[0]; // ���������� �������� ��������� ����������
-		y = z[1]; // 0 - ���������� x, 1 - ���������� �, 2 - ���������� z
+		x = z[0]; 
+		y = z[1]; 
+		Z = z[2]; 
+		
+		
+		F_PROFILE(f,thread,index) = Kq*q1*cos((PI*y)/(L+(2*d)));
+			 
+			}
+	end_f_loop(f,thread) 
+}
+
+
+
+DEFINE_PROFILE(heat_flux_2, thread, index) 
+{
+	real z[ND_ND]; 
+	real y; 
+	real x; 
+	real Z;
+	face_t f;
+
+	begin_f_loop(f,thread) 
+	{
+		F_CENTROID(z,f,thread);
+		x = z[0]; 
+		y = z[1];
 		Z = z[2];
 		
 		
 		F_PROFILE(f,thread,index) = Kq*q2*cos((PI*y)/(L+(2*d)));
 			 
 			}
-	end_f_loop(f,thread) // ����� �� ������������ �����
+	end_f_loop(f,thread) 
 }
 
-DEFINE_PROFILE(heat_flux_3, thread, index) // ���������� ����� �������������� ������� � �������
+DEFINE_PROFILE(heat_flux_3, thread, index)
 {
-	real z[ND_ND]; // ������, ���������� ���������� ����������� ������������
-	real y; // ���������� ���������� y ��� ���������� y
-	real x; // ���������� ���������� x ��� ���������� x
+	real z[ND_ND]; 
+	real y; 
+	real x; 
 	real Z;
 	face_t f;
 
-	begin_f_loop(f,thread) // ����������� ����
+	begin_f_loop(f,thread) 
 	{
 		F_CENTROID(z,f,thread);
-		x = z[0]; // ���������� �������� ��������� ����������
-		y = z[1]; // 0 - ���������� x, 1 - ���������� �, 2 - ���������� z
+		x = z[0]; 
+		y = z[1]; 
 		Z = z[2];
 		
 		
 		F_PROFILE(f,thread,index) = Kq*q3*cos((PI*y)/(L+(2*d)));
 			 
 			}
-	end_f_loop(f,thread) // ����� �� ������������ �����
+	end_f_loop(f,thread) 
 }
 
 
 
-DEFINE_PROFILE(heat_flux_4, thread, index) // ���������� ����� �������������� ������� � �������
+DEFINE_PROFILE(heat_flux_4, thread, index) 
 {
-	real z[ND_ND]; // ������, ���������� ���������� ����������� ������������
-	real y; // ���������� ���������� y ��� ���������� y
-	real x; // ���������� ���������� x ��� ���������� x
+	real z[ND_ND]; 
+	real y; 
+	real x; 
 	real Z;
 	face_t f;
 
-	begin_f_loop(f,thread) // ����������� ����
+	begin_f_loop(f,thread) 
 	{
 		F_CENTROID(z,f,thread);
-		x = z[0]; // ���������� �������� ��������� ����������
-		y = z[1]; // 0 - ���������� x, 1 - ���������� �, 2 - ���������� z
+		x = z[0]; 
+		y = z[1]; 
 		Z = z[2];
 		
 		
 		F_PROFILE(f,thread,index) = Kq*q4*cos((PI*y)/(L+(2*d)));
 			 
 			}
-	end_f_loop(f,thread) // ����� �� ������������ �����
+	end_f_loop(f,thread) 
 }
 
-DEFINE_PROFILE(heat_flux_5, thread, index) // ���������� ����� �������������� ������� � �������
+DEFINE_PROFILE(heat_flux_5, thread, index) 
 {
-	real z[ND_ND]; // ������, ���������� ���������� ����������� ������������
-	real y; // ���������� ���������� y ��� ���������� y
-	real x; // ���������� ���������� x ��� ���������� x
+	real z[ND_ND]; 
+	real y; 
+	real x; 
 	real Z;
 	face_t f;
 
-	begin_f_loop(f,thread) // ����������� ����
+	begin_f_loop(f,thread) 
 	{
 		F_CENTROID(z,f,thread);
-		x = z[0]; // ���������� �������� ��������� ����������
-		y = z[1]; // 0 - ���������� x, 1 - ���������� �, 2 - ���������� z
+		x = z[0]; 
+		y = z[1]; 
 		Z = z[2];
 		
 		
 		F_PROFILE(f,thread,index) = Kq*q5*cos((PI*y)/(L+(2*d)));
 			 
 			}
-	end_f_loop(f,thread) // ����� �� ������������ �����
+	end_f_loop(f,thread) 
 }
 
 
-DEFINE_PROFILE(heat_flux_6, thread, index) // ���������� ����� �������������� ������� � �������
+DEFINE_PROFILE(heat_flux_6, thread, index) 
 {
-	real z[ND_ND]; // ������, ���������� ���������� ����������� ������������
-	real y; // ���������� ���������� y ��� ���������� y
-	real x; // ���������� ���������� x ��� ���������� x
+	real z[ND_ND]; 
+	real y; 
+	real x; 
 	real Z;
 	face_t f;
 
-	begin_f_loop(f,thread) // ����������� ����
+	begin_f_loop(f,thread) 
 	{
 		F_CENTROID(z,f,thread);
-		x = z[0]; // ���������� �������� ��������� ����������
-		y = z[1]; // 0 - ���������� x, 1 - ���������� �, 2 - ���������� z
+		x = z[0]; 
+		y = z[1]; 
 		Z = z[2];
 		
 		
 		F_PROFILE(f,thread,index) = Kq*q6*cos((PI*y)/(L+(2*d)));
 			 
 			}
-	end_f_loop(f,thread) // ����� �� ������������ �����
+	end_f_loop(f,thread) 
 }
 
-DEFINE_PROFILE(heat_flux_7, thread, index) // ���������� ����� �������������� ������� � �������
+DEFINE_PROFILE(heat_flux_7, thread, index) 
 {
-	real z[ND_ND]; // ������, ���������� ���������� ����������� ������������
-	real y; // ���������� ���������� y ��� ���������� y
-	real x; // ���������� ���������� x ��� ���������� x
+	real z[ND_ND]; 
+	real y; 
+	real x; 
 	real Z;
 	face_t f;
 
-	begin_f_loop(f,thread) // ����������� ����
+	begin_f_loop(f,thread) 
 	{
 		F_CENTROID(z,f,thread);
-		x = z[0]; // ���������� �������� ��������� ����������
-		y = z[1]; // 0 - ���������� x, 1 - ���������� �, 2 - ���������� z
+		x = z[0]; 
+		y = z[1];
 		Z = z[2];
 		
 		
 		F_PROFILE(f,thread,index) = Kq*q7*cos((PI*y)/(L+(2*d)));
 			 
 			}
-	end_f_loop(f,thread) // ����� �� ������������ �����
+	end_f_loop(f,thread)
 }
 
 
-DEFINE_PROFILE(heat_flux_8, thread, index) // ���������� ����� �������������� ������� � �������
+DEFINE_PROFILE(heat_flux_8, thread, index) 
 {
-	real z[ND_ND]; // ������, ���������� ���������� ����������� ������������
-	real y; // ���������� ���������� y ��� ���������� y
-	real x; // ���������� ���������� x ��� ���������� x
+	real z[ND_ND]; 
+	real y; 
+	real x; 
 	real Z;
 	face_t f;
 
-	begin_f_loop(f,thread) // ����������� ����
+	begin_f_loop(f,thread) 
 	{
 		F_CENTROID(z,f,thread);
-		x = z[0]; // ���������� �������� ��������� ����������
-		y = z[1]; // 0 - ���������� x, 1 - ���������� �, 2 - ���������� z
+		x = z[0]; 
+		y = z[1]; 
 		Z = z[2];
 		
 		
 		F_PROFILE(f,thread,index) = Kq*q8*cos((PI*y)/(L+(2*d)));
 			 
 			}
-	end_f_loop(f,thread) // ����� �� ������������ �����
+	end_f_loop(f,thread) 
 }
 
 
 
 
-DEFINE_PROFILE(Coolant_Temp, thread, index) // ���������� ����� �������������� ������� � �������
+DEFINE_PROFILE(Coolant_Temp, thread, index) 
 {
-	real z[ND_ND]; // ������, ���������� ���������� ����������� ������������
+	real z[ND_ND]; 
 	
 	face_t f;
 
-	begin_f_loop(f,thread) // ����������� ����
+	begin_f_loop(f,thread) 
 	{
 		F_CENTROID(z,f,thread);
-		F_PROFILE(f,thread,index) = Kt*Tc; // ��������� �������
+		F_PROFILE(f,thread,index) = Kt*Tc; 
 	}
-	end_f_loop(f,thread) // ����� �� ������������ �����
+	end_f_loop(f,thread) 
 }
 
 
-DEFINE_PROFILE(Mass_Flow_distr_11, thread, index) // ���������� ����� �������������� ������� � �������
+DEFINE_PROFILE(Mass_Flow_distr_11, thread, index) 
 {
-	real z[ND_ND]; // ������, ���������� ���������� ����������� ������������
-	real y; // ���������� ���������� y ��� ���������� y
-	real x; // ���������� ���������� x ��� ���������� x
+	real z[ND_ND]; 
+	real y; 
+	real x; 
 	face_t f;
 
-	begin_f_loop(f,thread) // ����������� ����
+	begin_f_loop(f,thread) 
 	{
 		F_CENTROID(z,f,thread);
 	
-		F_PROFILE(f,thread,index) = Kg*G; // ��������� �������
+		F_PROFILE(f,thread,index) = Kg*G; 
 	}
-	end_f_loop(f,thread) // ����� �� ������������ �����
+	end_f_loop(f,thread)
 }
 
 	
